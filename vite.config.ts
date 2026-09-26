fcedfc4805afcf2a9747ea8b424777967bed3d4c
Skitarii11 <jawhaa_1107@yahoo.com>
2026-09-26 23:05:04 +0800

    asdasd8wehuqweasdadas

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     tanstackStart(),
+    nitro(),
     tailwindcss(),
     viteReact(),
   ],
