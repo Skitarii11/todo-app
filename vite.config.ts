fac9b9f772259cbd58e5d3c5aef3a727d5abd1df
Skitarii11 <jawhaa_1107@yahoo.com>
2026-09-26 22:32:05 +0800

    dsaasdasdasdaasdasdasd

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
-import { devtools } from '@tanstack/devtools-vite'
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     tanstackStart(),
-    devtools(),
     tailwindcss(),
     viteReact(),
   ],
